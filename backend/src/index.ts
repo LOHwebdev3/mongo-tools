662a1c5f7d6de8a3d27ae59063134494604f3019
herk <iherk.dev@gmail.com>
2025-06-04 08:49:18 +0700

    commit

---
+++ backend/src/index.ts
@@ -0,0 +1,134 @@
+import express from 'express';
+import cors from 'cors';
+import {MongoClient} from 'mongodb'
+import {spawn} from "child_process";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from Express + TypeScript!' });
+});
+
+app.post('/api/all-collections', async (req, res) => {
+    const uri = `mongodb://${req.body.MONGODB_USER}:${req.body.MONGODB_PASS}@${req.body.MONGODB_HOST}:${req.body.MONGODB_PORT}/${req.body.MONGODB_DB}?authSource=${req.body.MONGODB_DB_AUTH}`
+    const client = new MongoClient(uri)
+    await client.connect().then(()=>{
+        console.debug(`Connected to MongoDB`);
+    });
+    const db = client.db(req.body.MONGODB_DB);
+    const collections = await db.listCollections().toArray();
+    const cols= collections.map((col) => col.name)
+    res.json({ data:cols??[] });
+});
+
+
+app.post('/api/dump', async (req, res) => {
+    console.debug(`req.body => `, req.body);
+
+    if (req.body.selects && Array.isArray(req.body.selects)&&req.body.selects.length > 0) {
+        for (const col of req.body.selects) {
+            if (col) {
+                await new Promise<void>((resolve, reject) => {
+                    // สร้างโปรเซส mongodump พร้อมอาร์กิวเมนต์
+                    const backupProcess = spawn('mongodump', [
+                        '--gzip',
+                        `--uri="mongodb://${req.body.MONGODB_USER}:${req.body.MONGODB_PASS}@${req.body.MONGODB_HOST}:${req.body.MONGODB_PORT}/${req.body.MONGODB_DB}?authSource=${req.body.MONGODB_DB_AUTH}"`,
+                        `--collection=${col}`,
+                        `--out=${req.body.folder}` // ใช้โฟลเดอร์ที่ส่งมาจาก client
+                    ]);
+
+                    backupProcess.stdout.on('data', (data) => {
+                        console.log(`stdout: ${data}`);
+                    });
+
+                    backupProcess.stderr.on('data', (data) => {
+                        console.error(`stderr: ${data}`);
+                    });
+
+                    backupProcess.on('exit', (code, signal) => {
+                        if (code) {
+                            console.error('mongodump จบการทำงานพร้อมรหัส exit code = ' + code);
+                        } else if (signal) {
+                            console.error('mongodump ถูกหยุดด้วยสัญญาณ = ' + signal);
+                        } else {
+                            console.log(`dump ${col} finished`);
+                        }
+                        resolve()
+
+                    });
+                })
+            }
+        }
+
+        console.log('All selected collections have been dumped successfully.');
+
+    }else {
+
+        const backupProcess = spawn('mongodump', [
+            '--gzip',
+            `--uri="mongodb://${req.body.MONGODB_USER}:${req.body.MONGODB_PASS}@${req.body.MONGODB_HOST}:${req.body.MONGODB_PORT}/${req.body.MONGODB_DB}?authSource=${req.body.MONGODB_DB_AUTH}"`,
+            `--out=${req.body.folder}` // ใช้โฟลเดอร์ที่ส่งมาจาก client
+        ]);
+
+        backupProcess.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        backupProcess.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        backupProcess.on('exit', (code, signal) => {
+            if (code) {
+                console.error('mongodump จบการทำงานพร้อมรหัส exit code = ' + code);
+            } else if (signal) {
+                console.error('mongodump ถูกหยุดด้วยสัญญาณ = ' + signal);
+            } else {
+                console.log('dump finished');
+            }
+        });
+
+    }
+
+    res.json({ data:[] });
+});
+
+
+app.post('/api/restore', async (req, res) => {
+    console.debug(`req.body => `, req.body);
+    const restoreProcess = spawn('mongorestore', [
+        `--uri="mongodb://${req.body.MONGODB_USER}:${req.body.MONGODB_PASS}@${req.body.MONGODB_HOST}:${req.body.MONGODB_PORT}/${req.body.MONGODB_DB}?authSource=${req.body.MONGODB_DB_AUTH}"`,
+        `${req.body.folder}/`,
+        '--gzip',
+        '--drop',
+
+    ]);
+    //
+    restoreProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    restoreProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    restoreProcess.on('exit', (code, signal) => {
+        if (code) {
+            console.error('mongorestore จบการทำงานพร้อมรหัส exit code = ' + code);
+        } else if (signal) {
+            console.error('mongorestore ถูกหยุดด้วยสัญญาณ = ' + signal);
+        } else {
+            console.log('restore finished');
+        }
+    });
+
+    res.json({ data:[] });
+
+});
+
+
+app.listen(4000, () => {
+    console.log('🚀 Server listening on http://localhost:4000');
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,167 @@
+import React, {useEffect, useState} from 'react';
+import _ from 'lodash';
+
+import './index.css'
+
+const App = () => {
+    const [collections, setCollections] = useState([]);
+    const [selects, setSelects] = useState(JSON.parse(localStorage.getItem('select') as string)??[]);
+    const [folder, setFolder] = useState('dump');
+    const [stateA, setStateA] = useState(JSON.parse(localStorage.getItem('stateA') as string)??{
+        MONGODB_HOST:'',
+        MONGODB_PORT:'',
+        MONGODB_USER:'',
+        MONGODB_PASS:'',
+        MONGODB_DB_AUTH:'',
+        MONGODB_DB:'',
+    });
+    const [stateB, setStateB] = useState(
+        JSON.parse(localStorage.getItem('stateB') as string)??{
+        MONGODB_HOST:'',
+        MONGODB_PORT:'',
+        MONGODB_USER:'',
+        MONGODB_PASS:'',
+        MONGODB_DB_AUTH:'',
+        MONGODB_DB:'',
+    });
+
+    const onDump = async () => {
+
+        const dateString = new Date().toISOString().replace(/[:.]/g, '-');
+        console.debug(`dateString => `, dateString);
+        setFolder(dateString)
+
+        const res = await fetch('http://localhost:4000/api/dump', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({...stateA,folder:dateString,selects})
+        });
+        console.debug(`res => `, res);
+        if (localStorage){
+            localStorage.setItem('stateA', JSON.stringify(stateA));
+            localStorage.setItem('stateB', JSON.stringify(stateB));
+        }
+
+    }
+    const onRestore = async () => {
+        const res = await fetch('http://localhost:4000/api/restore', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({...stateB,folder:folder+'/'+stateA.MONGODB_DB})
+        });
+        console.debug(`res => `, res);
+        if (localStorage){
+            localStorage.setItem('stateA', JSON.stringify(stateA));
+            localStorage.setItem('stateB', JSON.stringify(stateB));
+        }
+    }
+
+
+    const getAllCollections = async (state: any) => {
+        const res = await fetch('http://localhost:4000/api/all-collections', {
+            body:JSON.stringify({...state,folder})
+            , method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+
+        const json = await res.json();
+        setCollections((json.data ?? []).sort((a: string, b: string) => a.localeCompare(b)));
+
+    }
+
+
+
+    // @ts-ignore
+    useEffect( async () => {
+        await getAllCollections(stateA)
+    }, [stateA]);
+
+
+
+    return(<div className={'w-screen h-screen flex flex-col'}>
+    <div className={'w-full h-fit flex flex-row p-2 border border-solid border-black gap-1'}>
+      <div className={'flex flex-col items-start w-1/2 h-full p-2 gap-0.5  border border-solid border-black rounded-lg'}>
+          <p>Host A</p>
+          <hr/>
+          Host
+          <input name={'MONGODB_HOST'} className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Host'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_HOST}/>
+          Port
+          <input name={'MONGODB_PORT'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Port'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_PORT}/>
+          User
+          <input name={'MONGODB_USER'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'User'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_USER}/>
+          Pass
+          <input name={'MONGODB_PASS'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Pass'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_PASS}/>
+          DB Auth
+          <input name={'MONGODB_DB_AUTH'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'DB Auth'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_DB_AUTH} />
+          DB
+          <input name={'MONGODB_DB'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'DB'} onChange={(e) => setStateA({...stateA, [e.target.name]: e.target.value})} value={stateA.MONGODB_DB}/>
+      </div>
+        <div className={'flex flex-col items-start w-1/2 h-full p-2 gap-0.5  border border-solid border-black rounded-lg'}>
+            <p>Host B</p>
+            <hr/>
+            Host
+            <input name={'MONGODB_HOST'} className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Host'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_HOST}/>
+            Port
+            <input name={'MONGODB_PORT'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Port'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_PORT}/>
+            User
+            <input name={'MONGODB_USER'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'User'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_USER}/>
+            Pass
+            <input name={'MONGODB_PASS'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'Pass'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_PASS}/>
+            DB Auth
+            <input name={'MONGODB_DB_AUTH'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'DB Auth'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_DB_AUTH} />
+            DB
+            <input name={'MONGODB_DB'}className={'w-full h-full border border-solid border-black rounded-lg p-1'} placeholder={'DB'} onChange={(e) => setStateB({...stateB, [e.target.name]: e.target.value})} value={stateB.MONGODB_DB}/>
+        </div>
+    </div>
+        <div className={'flex flex-row items-start gap-1 p-1'}>
+            Folder : {folder.toString()}
+
+        </div>
+        <div className={'flex flex-row gap-1 p-1'}>
+            <button onClick={onDump} className={'cursor-pointer bg-green-500 text-white p-1 rounded-lg hover:bg-green-600 transition-colors duration-300'}>
+                Dump
+            </button>
+            <button onClick={onRestore} className={'cursor-pointer bg-blue-500 text-white p-1 rounded-lg hover:bg-blue-600 transition-colors duration-300'}>
+                Restore
+            </button>
+
+            <button onClick={()=>{
+                setSelects(_.sortedUniq(collections));
+                localStorage.setItem('select', JSON.stringify(_.sortedUniq(collections)));
+            }} className={'cursor-pointer bg-red-500 text-white p-1 rounded-lg hover:bg-red-600 transition-colors duration-300'}>
+                Select All
+            </button>
+            <button onClick={()=>{
+                setSelects([]);
+                localStorage.setItem('select', JSON.stringify([]));
+            }} className={'cursor-pointer bg-orange-500 text-white p-1 rounded-lg hover:bg-orange-600 transition-colors duration-300'}>
+                Deselect All
+            </button>
+
+        </div>
+      <div className={'w-full h-full flex flex-row flex-wrap  gap-1 p-1'}>
+          {
+              collections.map((col, index) => (
+                    <div key={index} className={`cursor-pointer w-fit h-fit p-1 border border-solid border-black rounded-lg ${selects.includes(col)?'bg-green-500':'bg-white'}`} onClick={()=>{
+                        if (selects.includes(col)) {
+                            setSelects(selects.filter((item:string) => item !== col));
+                            localStorage.setItem('select', JSON.stringify(selects.filter((item:string) => item !== col)));
+                        } else {
+                            setSelects([...selects, col]);
+                            localStorage.setItem('select', JSON.stringify([...selects, col]));
+                        }
+                    }}>
+                        {col}
+                    </div>
+                ))
+          }
+      </div>
+  </div>);
+};
+export default App;
